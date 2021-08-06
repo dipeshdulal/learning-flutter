3efaddd1ff3eaac8db25a6ea64bd61729203f174
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-08-06 14:38:49 +0545

    feat: app router udpates

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,11 +1,27 @@
 import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter_rnd/router/guards/auth_guard.dart';
 import 'package:flutter_rnd/screens/home_screen.dart';
+import 'package:flutter_rnd/screens/login_screen.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: "Screen,Route",
   routes: <AutoRoute>[
-    AutoRoute(page: HomeScreen, initial: true, path: "/"),
+    AutoRoute(
+      page: EmptyRouterScreen,
+      path: "/",
+      initial: true,
+      name: "MainRoute",
+      guards: [AuthGuard],
+      children: [
+        AutoRoute(
+          page: HomeScreen,
+          path: "home",
+          initial: true,
+        ),
+      ],
+    ),
+    AutoRoute(page: LoginScreen, path: "/login"),
   ],
 )
 class $AppRouter {}
--- lib/router/app_router.gr.dart
+++ lib/router/app_router.gr.dart
@@ -7,28 +7,74 @@
 import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
-import '../screens/home_screen.dart' as _i3;
+import '../screens/home_screen.dart' as _i5;
+import '../screens/login_screen.dart' as _i4;
+import 'guards/auth_guard.dart' as _i3;
 
 class AppRouter extends _i1.RootStackRouter {
-  AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
+  AppRouter(
+      {_i2.GlobalKey<_i2.NavigatorState>? navigatorKey,
+      required this.authGuard})
       : super(navigatorKey);
+
+  final _i3.AuthGuard authGuard;
 
   @override
   final Map<String, _i1.PageFactory> pagesMap = {
+    MainRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (_) {
+          return const _i1.EmptyRouterScreen();
+        }),
+    LoginRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args =
+              data.argsAs<LoginRouteArgs>(orElse: () => const LoginRouteArgs());
+          return _i4.LoginScreen(key: args.key);
+        }),
     HomeRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
         routeData: routeData,
         builder: (_) {
-          return const _i3.HomeScreen();
+          return const _i5.HomeScreen();
         })
   };
 
   @override
-  List<_i1.RouteConfig> get routes =>
-      [_i1.RouteConfig(HomeRoute.name, path: '/')];
+  List<_i1.RouteConfig> get routes => [
+        _i1.RouteConfig(MainRoute.name, path: '/', guards: [
+          authGuard
+        ], children: [
+          _i1.RouteConfig('#redirect',
+              path: '', redirectTo: 'home', fullMatch: true),
+          _i1.RouteConfig(HomeRoute.name, path: 'home')
+        ]),
+        _i1.RouteConfig(LoginRoute.name, path: '/login')
+      ];
+}
+
+class MainRoute extends _i1.PageRouteInfo {
+  const MainRoute({List<_i1.PageRouteInfo>? children})
+      : super(name, path: '/', initialChildren: children);
+
+  static const String name = 'MainRoute';
+}
+
+class LoginRoute extends _i1.PageRouteInfo<LoginRouteArgs> {
+  LoginRoute({_i2.Key? key})
+      : super(name, path: '/login', args: LoginRouteArgs(key: key));
+
+  static const String name = 'LoginRoute';
+}
+
+class LoginRouteArgs {
+  const LoginRouteArgs({this.key});
+
+  final _i2.Key? key;
 }
 
 class HomeRoute extends _i1.PageRouteInfo {
-  const HomeRoute() : super(name, path: '/');
+  const HomeRoute() : super(name, path: 'home');
 
   static const String name = 'HomeRoute';
 }
