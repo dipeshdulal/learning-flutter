3ca782659b90c3e1b4792f5cd3e104de73081835
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-08-06 12:15:19 +0545

    feat: firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rnd/router/app_router.gr.dart';
 
@@ -7,14 +9,44 @@ void main() {
 
 final _appRouter = AppRouter();
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routeInformationParser: _appRouter.defaultRouteParser(),
-      routerDelegate: _appRouter.delegate(),
+      home: FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              body: Center(
+                  child: Text(
+                      "Something wrong happened while iniliazing firebase 🔥: \n" +
+                          snapshot.error.toString())),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp.router(
+              routeInformationParser: _appRouter.defaultRouteParser(),
+              routerDelegate: _appRouter.delegate(),
+            );
+          }
+
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+        future: _initialization,
+      ),
     );
   }
 }
