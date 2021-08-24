21606df9febf40e18a23f41fb4aa7abcaea9d8e7
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-08-24 13:30:38 +0545

    chore: added android side of MethodChannel

--- android/app/src/main/java/com/dipesh/test/flutter_rnd/MainActivity.java
+++ android/app/src/main/java/com/dipesh/test/flutter_rnd/MainActivity.java
@@ -1,6 +1,41 @@
 package com.dipesh.test.flutter_rnd;
 
+import android.os.BatteryManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "flutter.dev/home-screen";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        MethodChannel channel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
+        channel.setMethodCallHandler((call, result) -> {
+            if (call.method.equals("getBatteryLevel")) {
+                int level = getBatteryLevel();
+                if (level != -1) {
+                    result.success(level);
+                }else {
+                    result.error("UNAVAILABLE", "Battery level not available", null);
+                }
+            }else {
+                result.notImplemented();
+            }
+        });
+    }
+
+    private int getBatteryLevel(){
+        int batteryLevel = -1;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        }
+        return batteryLevel;
+    }
 }
